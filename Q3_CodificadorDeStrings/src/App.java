4ae6f7911c823786da1a621b662ca1ec76952691
Endrew <endrewmss@hotmail.com>
2024-08-31 23:35:36 -0300

    Q3 solved

--- Q3_CodificadorDeStrings/src/App.java
+++ Q3_CodificadorDeStrings/src/App.java
@@ -1,17 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Codificador cod = new Codificador();
-
+        CodificadorSimples cd_simples = new CodificadorSimples();
+        CodificaDesloca cd_desloca = new CodificaDesloca();
         String aux = "Desafio Padrões de Projeto";
 
         System.out.println(aux);
-        aux = cod.codifica(Codificador.Tipo.SIMPLES, aux);
+        aux = cod.codifica(cd_simples, aux);
         System.out.println(aux);
-        aux = cod.deCodifica(Codificador.Tipo.SIMPLES, aux);
+        aux = cod.deCodifica(cd_simples, aux);
         System.out.println(aux);
-        aux = cod.codifica(Codificador.Tipo.DESLOCA, aux);
+        aux = cod.codifica(cd_desloca, aux);
         System.out.println(aux);
-        aux = cod.deCodifica(Codificador.Tipo.DESLOCA, aux);
+        aux = cod.deCodifica(cd_desloca, aux);
         System.out.println(aux);
     }
 }
