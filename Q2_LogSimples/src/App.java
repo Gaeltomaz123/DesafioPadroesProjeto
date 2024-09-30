a4b3debb1f851633393fc9419a00ee5b68b40bc0
Endrew <endrewmss@hotmail.com>
2024-09-30 06:21:20 -0300

    casos de teste utilizando o padrao singleton

--- Q2_LogSimples/src/App.java
+++ Q2_LogSimples/src/App.java
@@ -1,12 +1,20 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         LogSimples log = LogSimples.getInstance();
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
-    
+
+        LogSimples log2 = LogSimples.getInstance();
+        log2.log("Finalmente o teste esta verificando o padrão SINGLETON!");
+
+
         for(String m:log){
             System.out.println(m);
         }
+
+        System.out.println("As duas instancias sao do mesmo objeto? " + (log == log2));
     }
 }
